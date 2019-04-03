5560426dd7145eac637df03e82539ba0214d0d97
JOHNSL <johnasuarezl@gmail.com>
2019-04-03 16:51:52 -0500

    Marcador

--- js/api.js
+++ js/api.js
@@ -4,5 +4,24 @@ function initMap() {
         center: {lat: 43.5293101, lng: -5.6773233},
         zoom: 13
     });
+    
+    var marker = new google.maps.Marker({
+        position: {lat: 4.642308, lng: -74.054125},
+        map: map,
+        title: 'Universidad Manuela Beltran'
+    });
 }
 
+
+
+
+// Register Service Worker
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      // Registration was successful
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+}
---
+++ js/service-worker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'api-google-maps';
+var urlsToCache = [
+  '/',
+  '../css/api.css',
+  '../index.html'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Finally active. Ready to start serving content!');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
